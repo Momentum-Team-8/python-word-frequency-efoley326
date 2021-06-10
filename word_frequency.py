694bc5a6817ce94a15cb1b4eff7b8ffe5a47ed0b
Emily Foley <efoley326@gmail.com>
2021-06-10 09:51:42 -0400

    debugging help from roen&greg

--- word_frequency.py
+++ word_frequency.py
@@ -1,46 +1,41 @@
-pythSTOP_WORDS = [
+STOP_WORDS = [
     'a', 'an', 'and', 'are', 'as', 'at', 'be', 'by', 'for', 'from', 'has', 'he',
     'i', 'in', 'is', 'it', 'its', 'of', 'on', 'that', 'the', 'to', 'were',
     'will', 'with'
 ]
 import string
 def print_word_freq(file):
-    with open(file) as one_today:
-        text = one_today.readwords()
+    with open(file) as file:
+        text = file.readlines()
     
     for words in text:
         words = words.lower()
         words = words.split()
-        words = words.strip('''!()-[]{};:'"\, <>./?@#$%^&*_~-''')
     words_to_keep = ""
-    for words in file:
+    for words in text:
         if words == STOP_WORDS:
             words == None
-    return words_to_keep
-
-
-def count_words(dict):
-    for words in print_word_freq:
+    for words in text:
         a = { words } 
     for words in a:
         if words in a:
             a[words] = a[words] + 1
-    sort_a = sorted(a.items(), key=lambda x: x[1], reverse=True)
-    for words in sort_a:
-	    print(words[0], words[1])
+    print (sorted(a.items(), key=lambda x: x[1], reverse=True))
+    # for words in sorted:
+	#     print(words[0], words[1])
 
-# if __name__ == "__main__":
-#     import argparse
-#     from pathlib import Path
+if __name__ == "__main__":
+    import argparse
+    from pathlib import Path
 
-#     parser = argparse.ArgumentParser(
-#         description='Get the word frequency in a text file.')
-#     parser.add_argument('file', help='file to read')
-#     args = parser.parse_args()
+    parser = argparse.ArgumentParser(
+        description='Get the word frequency in a text file.')
+    parser.add_argument('file', help='file to read')
+    args = parser.parse_args()
 
-#     file = Path(args.file)
-#     if file.is_file():
-#         print_word_freq(file)
-#     else:
-#         print(f"{file} does not exist!")
-#         exit(1)
+    file = Path(args.file)
+    if file.is_file():
+        print_word_freq(file)
+    else:
+        print(f"{file} does not exist!")
+        exit(1)
